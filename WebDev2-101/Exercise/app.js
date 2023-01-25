dac0d29964ed53f30e11dc5c30d66bb689ece848
sh310310 <110559375+sh310310@users.noreply.github.com>
2023-01-25 10:04:30 -0800

    edit

--- WebDev2-101/Exercise/app.js
+++ WebDev2-101/Exercise/app.js
@@ -2,42 +2,39 @@
 const form = document.getElementById("add");
 const input = document.getElementById("add-input");
 const ul = document.getElementById("list");
+const li = document.createElement("li");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   add();
 });
 
+//--------------------------------------
+// function add(note) {
+//   let noteText = input.value;
+
+//   li.innerHTML = `<p>${noteText}</p>
+//                   <p>
+//                     <i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i
+//                     </p>
+//                     <input type="text" class="edit-note">`;
+//   ul.appendChild(li);
+//   noteText = "";
+// }
+//--------------------------------------
+
 function add(note) {
   let noteText = input.value;
 
-  const list = document.getElementById("list");
-  const li = document.createElement("li");
-  li.innerHTML = `<p>${noteText}</p>
-                  <p>
-                    <i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i
-                    </p>
-                    <input type="text" class="edit-note">`;
-  list.appendChild(li);
-  noteText = "";
+  if (noteText) {
+    const li = document.createElement("li");
+    li.innerHTML = `<p>${noteText}</p>
+    <p>
+      <i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i
+      </p>
+      <input type="text" class="edit-note">`;
+
+    ul.appendChild(li);
+    input.value = "";
+  }
 }
-
-//   if (noteText) {
-//     const p = document.createElement("p");
-//     p.innerText = noteText;
-//     p.classList.add("firstP");
-
-//     // p.innerHTML = `<p>${textText}</p>
-//     // <p>
-//     //   <i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i
-//     //   </p>
-//     //   <input type="text" class="edit-note">`;
-
-//     // const pp = li.appendChild(p);
-//     li.appendChild(p);
-//     list.appendChild(li);
-//     input.value = "";
-// }
-
-// if (!value) return
-// const inputValue=event.target[0].value
