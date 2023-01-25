6479eb71eac5937fe19938f3687eb0e833166b5d
sh310310 <110559375+sh310310@users.noreply.github.com>
2023-01-25 09:53:05 -0800

    edited

--- WebDev2-101/Exercise/app.js
+++ WebDev2-101/Exercise/app.js
@@ -2,25 +2,42 @@
 const form = document.getElementById("add");
 const input = document.getElementById("add-input");
 const ul = document.getElementById("list");
-// const li = document.querySelector("li");
-const template = document.querySelector("li");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log();
   add();
 });
 
-function add(text) {
-  let textText = input.value;
+function add(note) {
+  let noteText = input.value;
 
-  if (textText) {
-    const p = document.createElement("li");
-
-    p.innerText = textText;
-
-    // const pp = li.appendChild(p);
-    input.value = "";
-  }
-  ul.appendChild(p);
+  const list = document.getElementById("list");
+  const li = document.createElement("li");
+  li.innerHTML = `<p>${noteText}</p>
+                  <p>
+                    <i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i
+                    </p>
+                    <input type="text" class="edit-note">`;
+  list.appendChild(li);
+  noteText = "";
 }
+
+//   if (noteText) {
+//     const p = document.createElement("p");
+//     p.innerText = noteText;
+//     p.classList.add("firstP");
+
+//     // p.innerHTML = `<p>${textText}</p>
+//     // <p>
+//     //   <i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i
+//     //   </p>
+//     //   <input type="text" class="edit-note">`;
+
+//     // const pp = li.appendChild(p);
+//     li.appendChild(p);
+//     list.appendChild(li);
+//     input.value = "";
+// }
+
+// if (!value) return
+// const inputValue=event.target[0].value
