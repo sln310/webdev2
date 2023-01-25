ff4f7f360dc7f6f16c7cb8e66817a51233340eff
sh310310 <110559375+sh310310@users.noreply.github.com>
2023-01-25 02:56:35 -0800

    done

--- WebDev2-101/Exercise/app.js
+++ WebDev2-101/Exercise/app.js
@@ -2,7 +2,8 @@
 const form = document.getElementById("add");
 const input = document.getElementById("add-input");
 const ul = document.getElementById("list");
-const p = document.getElementById("p");
+// const li = document.getElementById("li");
+// const p = document.getElementById("p");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -10,37 +11,32 @@ form.addEventListener("submit", function (event) {
   add();
 });
 
-function add(todo) {
-  let todoText = input.value;
+function add(text) {
+  let textText = input.value;
 
-  if (todo) {
-    todoText = todo.text;
+  if (text) {
+    textText = text.text;
   }
 
-  if (todoText) {
+  if (textText) {
     const li = document.createElement("li");
-    li.innerText = todoText;
-    li.classList.add("list-group-item");
-    // document.body.insertAfter(li, p);
 
-    if (todo && todo.completed) {
+    li.innerText = textText;
+    li.classList.add("list");
+
+    if (text && text.completed) {
       li.classList.add("text-decoration-line-through");
     }
 
     li.addEventListener("contextmenu", function (event) {
       event.preventDefault();
-      li.remove();
-      saveData();
     });
 
     li.addEventListener("click", function () {
       li.classList.toggle("text-decoration-line-through");
-      saveData();
     });
 
-    // document.body.insertAfter(li, p);
     ul.appendChild(li);
     input.value = "";
-    saveData();
   }
 }
